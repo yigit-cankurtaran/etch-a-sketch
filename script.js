b1e15d7dd7baa97e885b3ef3fc0a13ef509ed59e
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-03-05 16:40:54 +0300

    Made the grids wrap and change sizes with the window, I might be going in the wrong way though.

--- script.js
+++ script.js
@@ -6,8 +6,31 @@ document.addEventListener("DOMContentLoaded", () => {
     document.body.appendChild(gridContainer); // problematic line, uncaught typeerror
   }
 
-  // make the divs appear as a 16x16 grid
+  // create the individual grid items and append them to the grid container
+  for (let i = 0; i < 16; i++) {
+    const gridItem = document.createElement("div");
+    gridItem.classList.add("grid-item");
+    // adding them to the item class
+    gridContainer.appendChild(gridItem);
+  }
+
+  // set the dimensions and styles of the grid items
+  const gridItems = document.querySelectorAll(".grid-item");
+  gridItems.forEach((gridItem) => {
+    gridItem.style.flex = "1 0 auto";
+    gridItem.style.width = "100px";
+    gridItem.style.height = "100px";
+    gridItem.style.border = "1px solid black";
+  });
+
+  // set the grid template and grid container styles
+  gridContainer.style.display = "grid";
   gridContainer.style.gridTemplateColumns = "repeat(16, 1fr)";
   gridContainer.style.gridTemplateRows = "repeat(16, 1fr)";
   gridContainer.style.gap = "1px";
+  gridContainer.style.gridAutoFlow = "dense";
+  gridContainer.style.overflow = "auto";
+  gridContainer.style.display = "flex";
+  gridContainer.style.flexWrap = "wrap";
+  gridContainer.style.width = "100%";
 });
