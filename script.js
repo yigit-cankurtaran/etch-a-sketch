eb9b455371ee85f0f1ddf51a519df927df38a570
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-03-05 16:23:58 +0300

    Fixed the grid container.

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-// let's see what works and what doesn't.
-console.log("Hello world!");
-// prints
+document.addEventListener("DOMContentLoaded", () => {
+  // this fixed it.
+  const gridContainer = document.createElement("div");
+  gridContainer.classList.add("grid-container");
+  if (document.body) {
+    document.body.appendChild(gridContainer); // problematic line, uncaught typeerror
+  }
 
-// create a div with a class of grid-container and append it to the body
-const gridContainer = document.createElement("div");
-gridContainer.classList.add("grid-container");
-console.log("hi");
-//prints
-document.body.appendChild(gridContainer); // problematic line, uncaught typeerror
-console.log("hi");
-// this doesn't print.
-
-// make the divs appear as a 16x16 grid
-gridContainer.style.gridTemplateColumns = "repeat(16, 1fr)";
-gridContainer.style.gridTemplateRows = "repeat(16, 1fr)";
-gridContainer.style.gap = "1px";
+  // make the divs appear as a 16x16 grid
+  gridContainer.style.gridTemplateColumns = "repeat(16, 1fr)";
+  gridContainer.style.gridTemplateRows = "repeat(16, 1fr)";
+  gridContainer.style.gap = "1px";
+});
