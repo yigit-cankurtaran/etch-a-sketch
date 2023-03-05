71aeb60b897f8f4f63964013ef26058187370cec
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-03-05 16:19:56 +0300

    Identified error. Moving in to fix.

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-// create a page with a 16x16 grid of square divs
+// let's see what works and what doesn't.
+console.log("Hello world!");
+// prints
 
 // create a div with a class of grid-container and append it to the body
 const gridContainer = document.createElement("div");
 gridContainer.classList.add("grid-container");
-document.body.appendChild(gridContainer);
+console.log("hi");
+//prints
+document.body.appendChild(gridContainer); // problematic line, uncaught typeerror
+console.log("hi");
+// this doesn't print.
 
 // make the divs appear as a 16x16 grid
 gridContainer.style.gridTemplateColumns = "repeat(16, 1fr)";
 gridContainer.style.gridTemplateRows = "repeat(16, 1fr)";
 gridContainer.style.gap = "1px";
-
-// show the grids on screen, different color for each
-for (let i = 0; i < 256; i++) {
-  const grid = document.createElement("div");
-  grid.classList.add("grid");
-  grid.style.backgroundColor = "black";
-  gridContainer.appendChild(grid);
-}
